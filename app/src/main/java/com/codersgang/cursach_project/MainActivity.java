0fb35ad5240c786438edb2beb3663f276e7e5e09
Alexandrus21a <alexandrus21dev@yahoo.com>
2022-04-21 17:23:01 +0300

    upd

---
+++ app/src/main/java/com/codersgang/cursach_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codersgang.cursach_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
