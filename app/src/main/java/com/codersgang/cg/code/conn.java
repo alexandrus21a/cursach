2903f39c9ae2bcc4aafa22d43a2db190ece1e302
Alexandrus21a <alexandrus21dev@yahoo.com>
2022-04-22 20:53:28 +0300

    конструктор для подключения

--- app/src/main/java/com/codersgang/cg/code/conn.java
+++ app/src/main/java/com/codersgang/cg/code/conn.java
@@ -3,11 +3,17 @@ package com.codersgang.cg.code;
 import java.sql.*;
 
 public class conn {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        conn c = new conn();
+        conn c2 = new conn();
+        c.connect("jdbc:mysql://localhost/cursach","","");
+        //c2.connect("jdbc:mysql://localhost/cursach","sanya","1111");
+    }
+    public static void connect(String link, String usr, String pass){
         try {
-            String url = "jdbc:mysql://localhost/cursach";
-            String username = "sanya";
-            String password = "1111";
+            String url = link;
+            String username = usr;
+            String password = pass;
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement statement = conn.createStatement();
