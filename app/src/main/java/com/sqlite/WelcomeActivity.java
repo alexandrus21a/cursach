95f00afc444734e09df1f12ec1142ef4303765c9
Alexandrus21a <alexandrus21dev@yahoo.com>
2022-05-20 13:22:40 +0300

    iii

---
+++ app/src/main/java/com/sqlite/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.sqlite;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button main_btn;
+    private Button sup_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        main_btn = findViewById(R.id.main_btn);
+        main_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.CountryListActivity);
+            }
+        });
+
+        sup_btn = findViewById(R.id.sup_btn);
+        sup_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.CountryListActivity);
+            }
+        });
+
+    }
+}
